7934773344c3f40f21e5fd5f2182c36bc7ba6971
Benjamin Staugaard <bemmamin@live.dk>
2018-11-05 21:55:40 +0100

    Module 17

--- src/module17javafx/Module17JavaFX.java
+++ src/module17javafx/Module17JavaFX.java
@@ -5,17 +5,94 @@
  */
 package module17javafx;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+import javafx.stage.Stage;
+
 /**
  *
  * @author Benjamin Staugaard | Benz56
  */
-public class Module17JavaFX {
+public class Module17JavaFX extends Application {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("fxml_example.fxml"));
+        
+        Scene scene = new Scene(root, 300, 275);
+        
+        primaryStage.setTitle("FXML Welcome");
+        primaryStage.setScene(scene);
+        primaryStage.show();
     }
     
+    /* Chapter 2:*/
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        final GridPane gridPane = new GridPane();
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setHgap(10);
+        gridPane.setVgap(10);
+        gridPane.setPadding(new Insets(25, 25, 25, 25));
+
+        final Text title = new Text("Welcome");
+        title.setFont(Font.font("Tahoma", FontWeight.NORMAL, 20));
+        gridPane.add(title, 0, 0, 2, 1);
+
+        gridPane.add(new Label("Username: "), 0, 1);
+        gridPane.add(new TextField(), 1, 1);
+        gridPane.add(new Label("Password: "), 0, 2);
+        gridPane.add(new PasswordField(), 1, 2);
+
+        final Button button = new Button("Sign in");
+        final HBox hBox = new HBox(10, button);
+        hBox.setAlignment(Pos.BOTTOM_RIGHT);
+        gridPane.add(hBox, 1, 4);
+
+        final Text actionTarget = new Text();
+        gridPane.add(actionTarget, 1, 6);
+
+        button.setOnAction(event -> {
+            actionTarget.setFill(Color.FIREBRICK);
+            actionTarget.setText("Sign in button pressed");
+        });
+
+        Scene scene = new Scene(gridPane, 300, 275);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    /* Chapter 1:*/
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        primaryStage.setTitle("Hello World");
+
+        final Button button = new Button("Say 'Hello World'");
+        button.setOnAction(event -> {
+            System.out.println("Hello World!");
+        });
+
+        final StackPane root = new StackPane(button);
+        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.show();
+    }
 }
